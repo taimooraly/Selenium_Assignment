6e876885df750535f51e33270cc63b576160612f
Syed Taimoor Ali <Syed Taimoor Ali@DESKTOP-VRG0UI9>
2023-04-14 00:43:24 +0500

    Add project files.

---
+++ Selenium_Assignment/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Selenium_Assignment")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Selenium_Assignment")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0d67ef1f-b0ee-4ce6-9a94-133490f65252")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Selenium_Assignment/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Security.Policy;
+
+namespace Selenium_Assignment
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        [DataRow("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login", "Admin", "admin123")]
+        
+        public void TestMethod1(string url, string user, string pass)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Url = url;
+            driver.FindElement(By.ClassName("oxd-input oxd-input--active")).SendKeys(user);
+            driver.FindElement(By.Name("password")).SendKeys(pass);
+            driver.FindElement(By.ClassName("oxd-button oxd-button--medium oxd-button--main orangehrm-login-button")).Click();
+            string newUrl = driver.Url;
+            Assert.AreEqual("https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index", newUrl);
+            driver.Close();
+        }
+    }
+} 
